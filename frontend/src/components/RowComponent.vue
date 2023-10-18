7e8ea11e427a809ea0d4dd91fc72ab8db5963600
FinnArtmann <92019544+FinnArtmann@users.noreply.github.com>
2023-10-18 16:08:17 +0000

    feat(frontend): Add home page skeleton
    Adds a vertically scrollable home page which consists of rows of horizontally sipeable cards.

---
+++ frontend/src/components/RowComponent.vue
@@ -0,0 +1,61 @@
+<template>
+  <swiper :slidesPerView="1.2" :centeredSlides="true" :spaceBetween="5">
+
+    <swiper-slide>
+      <ion-card class="full-width-card">
+        <ion-card-header>
+          <ion-card-title>Card Title</ion-card-title>
+          <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+        </ion-card-header>
+      </ion-card>
+
+    </swiper-slide>
+
+    <swiper-slide>
+      <ion-card class="full-width-card">
+        <ion-card-header>
+          <ion-card-title>Card Title</ion-card-title>
+          <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+        </ion-card-header>
+      </ion-card>
+
+    </swiper-slide>
+    
+  </swiper>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+import 'swiper/css';
+import '@ionic/vue/css/ionic-swiper.css';
+
+export default defineComponent({
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+});
+</script>
+
+<style scoped>
+.full-width-card {
+  width: 100%;
+  height: 95%;
+  background-color: var(--ion-color-light-shade);
+  border-radius: 20px;
+}
+
+.swiper-slide-active {
+  transform: scale(1.05, 1.05);
+}
+
+.swiper-slide {
+  transition: 400ms all ease-in-out;
+}
+
+.swiper{
+  padding-top:2%;
+}
+</style>
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -2,33 +2,37 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>OneShot</ion-title>
+        <ion-title size="small">Remember the happy days!</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
+    <ion-content :fullscreen="true"> 
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
+     
+      <row-component style="height: 70%;"></row-component>
+      <row-component style="height: 20%;"></row-component>
+      <row-component style="height: 50%;"></row-component>
+      <row-component style="height: 70%;"></row-component>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import RowComponent from '@/components/RowComponent.vue'
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -44,13 +48,16 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
 #container a {
   text-decoration: none;
 }
+
+
+
 </style>
