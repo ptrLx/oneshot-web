f779e194bd170331241d0c280bf92cbfef61fcb9
FinnArtmann <92019544+FinnArtmann@users.noreply.github.com>
2023-10-31 09:57:25 +0000

    feat(frontend): Add minimal pwa setup

--- README.md
+++ README.md
@@ -14,3 +14,18 @@
 ### Start the frontend
 
 1. `make start-frontend`
+
+
+### PWA Dev Setup
+
+1. `cd frontend`
+2. Make sure `devOptions { enabled: true}` is set in `vite.config.js`
+3. Run 'npm run build' to generate the service worker and manifest in the `dist` folder
+4. Run `npm run dev`, the pwa should now appear as installable in the browser
+
+#### Checking PWA requirements, usability and performance
+1. Install Chrome
+2. Install Google Lighthouse extension
+3. Open dev tools (F12) and go to the Lighthouse tab
+4. Run the audit
+
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,29 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "/icons/512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+        /* other options */
+      }
+    })
   ],
   resolve: {
     alias: {
