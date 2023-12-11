85535853e47ea759ef01fba0d38866e2f1e58778
FinnArtmann <92019544+FinnArtmann@users.noreply.github.com>
2023-12-11 20:01:54 +0000

    feat(frontend): Add calendar highlightedDates API call

--- frontend/src/components/CalendarComponent.vue
+++ frontend/src/components/CalendarComponent.vue
@@ -1,105 +1,110 @@
 <template>
-    <ion-datetime class="datetime" presentation="date" display-format="DDDD MMMM D, YYYY" picker-format="DDDD MMMM D, YYYY"
-        placeholder="Select Date" size="cover" :highlightedDates=highlightedDates></ion-datetime>
+    <ion-datetime id="calendar" class="datetime" presentation="date" display-format="DDDD MMMM D, YYYY"
+        picker-format="DDDD MMMM D, YYYY" placeholder="Select Date" size="cover" :highlightedDates="highlightedDates"
+        :value="selectedDate.toISOString()"></ion-datetime>
 </template>
   
 <script lang="ts">
-
-import { IonCard, IonButton, IonTitle, IonGrid, IonRow, IonCol, IonDatetime, IonItem, IonList } from '@ionic/vue';
-import { defineComponent } from 'vue';
-import { Happiness } from '@/types/Happiness';
-import { def } from '@vue/shared';
-import { DatetimeHighlight } from '@ionic/core';
-
+import { IonDatetime, onIonViewDidEnter } from '@ionic/vue';
+import { defineComponent, ref, onMounted, watch, nextTick } from 'vue';
+import { CalendarService } from '@/_generated/api-client';
+import { CalendarEntryRespDTO } from '@/_generated/api-client/models/CalendarEntryRespDTO';
+import { HappinessDTO } from '@/_generated/api-client/models/HappinessDTO';
 
 export default defineComponent({
     components: {
-        IonCard,
-        IonButton,
-        IonTitle,
-        IonGrid,
-        IonRow,
-        IonCol,
         IonDatetime,
-        IonItem,
-        IonList,
     },
     setup() {
-        let highlightedDates: DatetimeHighlight[];
-
-        // Dummy data until api call is implemented
-        const api_response =
-            [
-                {
-                    "date": "2023-11-16",
-                    "happiness_state": "VERY_HAPPY",
-                },
-                {
-                    "date": "2023-11-17",
-                    "happiness_state": "HAPPY",
-                },
-                {
-                    "date": "2023-11-18",
-                    "happiness_state": "NEUTRAL",
-                },
-                {
-                    "date": "2023-11-19",
-                    "happiness_state": "SAD",
-                },
-                {
-                    "date": "2023-11-20",
-                    "happiness_state": "VERY_SAD",
-                },
-                {
-                    "date": "2023-11-21",
-                    "happiness_state": "NOT_SPECIFIED",
-                },
-            ]
-
-        highlightedDates = api_response.map((item) => {
-            backgroundColor: ['#41B883', '#A1C349', '#ccffcc', '#F2C94C', '#cc0000']
-            let bgColor = "";
-            switch (item.happiness_state as Happiness) {
-                case Happiness.VERY_HAPPY:
-                    bgColor = "#41B883";
-                    break;
-                case Happiness.HAPPY:
-                    bgColor = "#A1C349";
-                    break;
-                case Happiness.NEUTRAL:
-                    bgColor = "#ccffcc";
-                    break;
-                case Happiness.SAD:
-                    bgColor = "#F2C94C";
-                    break;
-                case Happiness.VERY_SAD:
-                    bgColor = "#cc0000";
-                    break;
-                case Happiness.NOT_SPECIFIED:
-                    bgColor = "rgba(169, 169, 169, 0.2)";
-                    break;
-                default:
-                    bgColor = "rgba(169, 169, 169, 0.2)";
-                    break;
-            }
-
-            return {
-                date: item.date,
-                backgroundColor: bgColor
-            }
-        });
 
 
+        const selectedDate = new Date();
+        const displayedMonth = ref<string>('');
+        const highlightedDates = ref<{ date: string; textColor: string, backgroundColor: string }[]>();
+        const observeCalendarChanges = () => {
+            const targetNode = document.querySelector('ion-datetime#calendar');
+            if (!targetNode) return;
 
+            const observerCallback = (mutationsList: MutationRecord[]) => {
+                for (const mutation of mutationsList) {
+                    if (mutation.type === 'attributes') {
+                        const calendar = document.querySelector('ion-datetime#calendar');
+                        const label = calendar?.shadowRoot?.querySelector('.calendar-month-year ion-item ion-label');
+                        const textContent = label?.textContent;
+
+                        if (textContent && textContent !== displayedMonth.value) {
+                            displayedMonth.value = textContent
+                            handleMonthChange(textContent);
+                            console.log(textContent);
+                        }
+                    }
+                }
+            };
+
+            const observer = new MutationObserver(observerCallback);
+            observer.observe(targetNode, { attributes: true, childList: true, subtree: true });
+        };
+
+        onMounted(observeCalendarChanges);
+
+        const handleMonthChange = (dateText: string) => {
+            selectedDate.setMonth(new Date(`1 ${dateText}`).getMonth());
+            const formattedDate = formatDisplayDate(dateText);
+            CalendarService.getCalendarCalendarGet(formattedDate).then((response) => {
+                highlightedDates.value = updateHighlightedDates(response);
+            });
+        };
+
+        const formatDisplayDate = (dateText: string) => {
+            const date = new Date(`1 ${dateText}`);
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            return `${year}-${month.toString().padStart(2, '0')}`;
+        };
+
+        const updateHighlightedDates = (response: CalendarEntryRespDTO[]) => {
+            let result = response.map((item) => {
+                let bgColor = '';
+                const happinessState = item.oneshot?.happiness;
+                switch (happinessState) {
+                    case HappinessDTO.VERY_HAPPY:
+                        bgColor = 'var(--color-very-happy)';
+                        break;
+                    case HappinessDTO.HAPPY:
+                        bgColor = 'var(--color-happy)';
+                        break;
+                    case HappinessDTO.NEUTRAL:
+                        bgColor = 'var(--color-neutral)';
+                        break;
+                    case HappinessDTO.SAD:
+                        bgColor = 'var(--color-sad)';
+                        break;
+                    case HappinessDTO.VERY_SAD:
+                        bgColor = 'var(--color-very-sad)';
+                        break;
+                    default:
+                        bgColor = 'var(--color-unspecified)';
+                        break;
+                }
+
+                return {
+                    date: item.date,
+                    textColor: 'var(--ion-color-light)',
+                    backgroundColor: bgColor,
+                };
+            });
+
+            return result;
+        };
 
         return {
-
-            highlightedDates
-        }
-    }
+            highlightedDates,
+            selectedDate,
+        };
+    },
 });
-
 </script>
+  
 <style scoped>
 .datetime {
     min-height: 0px;
@@ -117,10 +122,8 @@ ion-datetime::part(calendar-day):focus {
 }
 
 /* 
-ion-datetime::part(calendar-day) {
+  ion-datetime::part(calendar-day) {
     border: 1px dashed var(--ion-color-primary);
-} */
+  } */
 </style>
-
-
-
+  
--- frontend/src/types/Happiness.ts
+++
@@ -1,8 +0,0 @@
-export enum Happiness {
-    VERY_HAPPY = "VERY_HAPPY",
-    HAPPY = "HAPPY",
-    NEUTRAL = "NEUTRAL",
-    SAD = "SAD",
-    VERY_SAD = "VERY_SAD",
-    NOT_SPECIFIED = "NOT_SPECIFIED"
-}
